cc0adee5eba64026efa261b88b7ccec0fdc43699
Jane Anjie Chen <50345192+JaneAnjieChen@users.noreply.github.com>
2022-11-24 12:39:06 -0800

    Delete Parameters.cs

--- UserInput/Parameters.cs
+++
@@ -1,9 +0,0 @@
-
-using System.Collections.Generic;
-
-[System.Serializable]
-public class Parameters
-{
-    public Dictionary<string, string> recordedAnswers = new Dictionary<string, string>();
-    //public string[] recordedAnswers;
-}
