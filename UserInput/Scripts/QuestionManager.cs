d6c3eaa7b84c73954276e4c97fde48c67f2f5c65
Jane Anjie Chen <50345192+JaneAnjieChen@users.noreply.github.com>
2022-11-26 15:34:15 -0800

    Update QuestionManager.cs

--- UserInput/Scripts/QuestionManager.cs
+++ UserInput/Scripts/QuestionManager.cs
@@ -12,6 +12,7 @@ using System.Threading.Tasks;
 using System.IO;
 using static System.Net.Mime.MediaTypeNames;
 using System.Threading;
+using System.Security.Cryptography;
 
 public class QuestionManager : MonoBehaviour
 {
@@ -20,7 +21,10 @@ public class QuestionManager : MonoBehaviour
     public int currentQuestion; // current question index
     public TextMeshProUGUI QuestionTxt;
 
+    //To store the user's input as a Dictionary
     public Parameters RecordedAnswers;
+    //To store the generated image as a Dictionary. Format: {int image_index: byte[] image}
+    public Dictionary<int, byte[]> GeneratedImages = new Dictionary<int, byte[]>();
 
     public GameObject QuestionPanel;
     public GameObject DisplayPanel;
@@ -42,51 +46,70 @@ public class QuestionManager : MonoBehaviour
     }
 
     // After the user's input, display the image
-    // function: disable the Question panel, enable the display panel
-    
-    async void DisplayImage(string ImagePath)
+    async void DisplayImage()
     {
-        
+        // function: disable the Question panel, enable the display panel
         QuestionPanel.SetActive(false);
         DisplayPanel.SetActive(true);
 
-        /*
-        while (File.Exists(ImagePath) == false)
-        {
-            Thread.Sleep(1000);
-        }
-        */
-
         // To make sure get the image first before display
-        await RetrieveGeneratedImages(RecordedAnswers, ImagePath);
-        
+        //await RetrieveGeneratedImages(RecordedAnswers);
 
-        if (File.Exists(ImagePath))
+        HttpClient client = new();
+        // Add our Auth token
+        client.DefaultRequestHeaders.Add("Authorization", "Bearer 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");
+        /* Prepare request's data
+         * Only generate 1 image for now
+         */
+        object data = new
         {
-            // 1.read the bytes array
-            byte[] bytes = File.ReadAllBytes(ImagePath);
-            // 2.create a texture
-            //The default size is not important since it will be overridden by the loading method.
-            Texture2D tex = new Texture2D(1, 1);
-            // 3.load inside tx the bytes and use the correct image size
-            tex.LoadImage(bytes);
+            prompt = ProcessUserInputs(RecordedAnswers),
+            n = 1,
+            size = "1024x1024",
+            response_format = "b64_json"
+        };
+        var myContent = JsonConvert.SerializeObject(data);
+        var buffer = System.Text.Encoding.UTF8.GetBytes(myContent);
+        var byteContent = new ByteArrayContent(buffer);
+        byteContent.Headers.ContentType = new MediaTypeHeaderValue("application/json");
+        // Send POST request
+        var response = await client.PostAsync("https://api.openai.com/v1/images/generations", byteContent);
+        // Convert response to JSON
+        var responseString = await response.Content.ReadAsStringAsync();
+        var dynamicResponseObject = JsonConvert.DeserializeObject<dynamic>(responseString)!;
+        /* b64_json.ToString() contains the base64 encoding of the generated image
+         */
+        string imageString = dynamicResponseObject.data[0].b64_json.ToString();
+        byte[] imageBytes = Convert.FromBase64String(imageString);
 
-            // 4.create a rect using the textute dimensions
-            Rect rec = new Rect(0, 0, tex.width, tex.height);
-            // 5. convert the texture in sprite
-            Sprite spriteToUse = Sprite.Create(tex, rec, new Vector2(0.5f, 0.5f), 100);
-            // 6.load the sprite used by UI Image
-            imageToDisplay.sprite = spriteToUse;
-        }
-        else
-        {
-            print("No Image File.");
-        }
+        //File.WriteAllBytes(savePath, imageBytes);
+        print("New Image Generation ends");
+
+        int ImageNum = GeneratedImages.Count;
+        // store the newly generated image into GeneratedImages
+        GeneratedImages[ImageNum + 1] = imageBytes;
+
+        // 1.read the bytes array
+        //byte[] bytes = File.ReadAllBytes(ImagePath);
+
+        // 2.create a texture
+        //The default size is not important since it will be overridden by the loading method.
+        Texture2D tex = new Texture2D(1, 1);
+        // 3.load inside tx the bytes and use the correct image size
+        tex.LoadImage(imageBytes);
+
+        // 4.create a rect using the textute dimensions
+        Rect rec = new Rect(0, 0, tex.width, tex.height);
+        // 5. convert the texture in sprite
+        Sprite spriteToUse = Sprite.Create(tex, rec, new Vector2(0.5f, 0.5f), 100);
+        // 6.load the sprite used by UI Image
+        imageToDisplay.sprite = spriteToUse;
+
     }
 
     void generateQuestion()
     {
-        if(QnA.Count > 0)
+        if (QnA.Count > 0)
         {
             currentQuestion = UnityEngine.Random.Range(0, QnA.Count);
 
@@ -95,25 +118,26 @@ public class QuestionManager : MonoBehaviour
         }
         else
         {
-            string BathImagePath = "Assets/Images/";
-            string[] dirs = System.IO.Directory.GetFileSystemEntries(BathImagePath);
-            int ImageNum = dirs.Length + 1;
+            // Images storage folder, need to change according to device
+            //string BathImagePath = "/sdcard/DCIM/Camera/";
+            //string[] dirs = System.IO.Directory.GetFileSystemEntries(BathImagePath);
+            //int ImageNum = dirs.Length + 1;
 
             //form image path
-            string ImagePath = BathImagePath + ImageNum.ToString() + ".png";
+            //string ImagePath = BathImagePath + ImageNum.ToString() + ".png";
 
             //RetrieveGeneratedImages(RecordedAnswers, ImagePath);
 
-            DisplayImage(ImagePath);
+            DisplayImage();
         }
     }
 
     void setAnswers()
     {
-        for(int i = 0; i < options.Length; i++)
+        for (int i = 0; i < options.Length; i++)
         {
             options[i].GetComponent<AnswerScript>().optionText = QnA[currentQuestion].Answers[i];
-            
+
             if (QnA[currentQuestion].Question.Contains("mood"))
             {
                 options[i].GetComponent<AnswerScript>().questionText = "Mood";
@@ -131,7 +155,7 @@ public class QuestionManager : MonoBehaviour
         }
     }
 
-    
+
     string ProcessUserInputs(Parameters userInputs)
     {
         var buildString = "Guns, soul, ruins,";
@@ -151,41 +175,4 @@ public class QuestionManager : MonoBehaviour
         return buildString;
     }
 
-    // POST
-    async Task<bool> RetrieveGeneratedImages(Parameters userInputs, string savePath)
-    {
-        HttpClient client = new();
-        // Add our Auth token
-        client.DefaultRequestHeaders.Add("Authorization", "Bearer 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");
-        /* Prepare request's data
-         * Only generate 1 image for now
-         */
-        object data = new
-        {
-            prompt = ProcessUserInputs(userInputs),
-            n = 1,
-            size = "1024x1024",
-            response_format = "b64_json"
-        };
-        var myContent = JsonConvert.SerializeObject(data);
-        var buffer = System.Text.Encoding.UTF8.GetBytes(myContent);
-        var byteContent = new ByteArrayContent(buffer);
-        byteContent.Headers.ContentType = new MediaTypeHeaderValue("application/json");
-        // Send POST request
-        var response = await client.PostAsync("https://api.openai.com/v1/images/generations", byteContent);
-        // Convert response to JSON
-        var responseString = await response.Content.ReadAsStringAsync();
-        var dynamicResponseObject = JsonConvert.DeserializeObject<dynamic>(responseString)!;
-        /* b64_json.ToString() contains the base64 encoding of the generated image
-         */
-        string imageString = dynamicResponseObject.data[0].b64_json.ToString();
-        byte[] imageBytes = Convert.FromBase64String(imageString);
-        // Replace with the appropriate path
-        File.WriteAllBytes(savePath, imageBytes);
-
-        //print("Save Image function ends");
-        return true;
-    }
-
-   
-}
+}  
